9fe33519766599d239418e364d5a7f1b0e3cb805
vlad1984 <vladimirbeauge@sunyorange.edu>
2026-04-01 10:53:05 -0400

    Create my_logger.py

---
+++ src/my_logger.py
@@ -0,0 +1,11 @@
+import logging
+
+logging.basicConfig(
+    filename="logs/lab1_test2.log",
+    level=logging.INFO,
+    format="%(asctime)s | %(levelname)s | %(message)s",
+    filemode="w"
+)
+
+log = logging.getLogger(__name__)
+log.info("Logger initialized")
