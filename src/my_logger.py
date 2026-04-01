716e31eb73bf1eff403d248ffade89bfbcb1f8a5
vlad1984 <vladimirbeauge@sunyorange.edu>
2026-04-01 11:26:16 -0400

    Update my_logger.py

--- src/my_logger.py
+++ src/my_logger.py
@@ -1,7 +1,7 @@
 import logging
 
 logging.basicConfig(
-    filename="logs/lab1_test3.log",
+    filename="../logs/lab1_test3.log",
     level=logging.INFO,
     format="%(asctime)s | %(levelname)s | %(message)s",
     filemode="w"
