06ac03d59fb02c8dade2ebed920ec9d95c363835
vlad1984 <vladimirbeauge@sunyorange.edu>
2026-04-01 11:18:19 -0400

    Update main.py

--- src/main.py
+++ src/main.py
@@ -8,13 +8,14 @@ file_names = [f for f in os.listdir(folder_path) if os.path.isfile(os.path.join(
 file_names = [f for f in file_names if f.rsplit('.')[-1].lower() == 'pdf']
 
 for fn in file_names:
-  reader = PdfReader(file_path)
+  reader = PdfReader(fn)
   num_pages = len(reader.pages)
 
   txt = ''
   for i in range(num_pages):
     pge = reader.pages[i]
-    txt += pge.extract_text()
+    t = pge.extract_text()
+    txt += t or ""
 
   # TODO - (PROCESS) each lab 'matched' lab
   file_prefix = fn.rsplit('.')[0].lower()
@@ -24,9 +25,9 @@ for fn in file_names:
       
       q1 = q1.replace('\n',' ')
       q1 = q1.lower()
-      check_flag = 'input' in q1 and 'process' in q1 and 'output' in q1
+      check_flag = all(word in q1 for word in ["input", "process", "output"])      
       
-      assert check_flag, "expecting mention of input/process/output"
-      print("q1, passed")
+      if check_flag:  log.error("expecting mention of input/process/output")
+      else:           log.info("q1, passed")
     case _:  
       log.error(f"{file_prefix} does not exist")
